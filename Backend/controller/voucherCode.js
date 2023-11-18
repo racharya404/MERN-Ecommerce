343a398fda128383ed739cf992b387b1ccc0b59a
racharya404 <rojanacharya404@gmail.com>
2023-11-18 12:23:06 +0545

    2nd Commit

--- Backend/controller/voucherCode.js
+++ Backend/controller/voucherCode.js
@@ -3,7 +3,7 @@ const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const Shop = require("../model/shop");
 const ErrorHandler = require("../utils/ErrorHandler");
 const { isSeller } = require("../middleware/auth");
-const CoupounCode = require("../model/coupounCode");
+const VoucherCode = require("../model/voucherCode");
 const router = express.Router();
 
 // create coupoun code
@@ -12,19 +12,19 @@ router.post(
   isSeller,
   catchAsyncErrors(async (req, res, next) => {
     try {
-      const isCoupounCodeExists = await CoupounCode.find({
+      const isVoucherCodeExists = await VoucherCode.find({
         name: req.body.name,
       });
 
-      if (isCoupounCodeExists.length !== 0) {
+      if (isVoucherCodeExists.length !== 0) {
         return next(new ErrorHandler("Coupoun code already exists!", 400));
       }
 
-      const coupounCode = await CoupounCode.create(req.body);
+      const voucherCode = await VoucherCode.create(req.body);
 
       res.status(201).json({
         success: true,
-        coupounCode,
+        voucherCode,
       });
     } catch (error) {
       return next(new ErrorHandler(error, 400));
@@ -38,7 +38,7 @@ router.get(
   isSeller,
   catchAsyncErrors(async (req, res, next) => {
     try {
-      const couponCodes = await CoupounCode.find({ shopId: req.seller.id });
+      const couponCodes = await VoucherCode.find({ shopId: req.seller.id });
       res.status(201).json({
         success: true,
         couponCodes,
@@ -55,7 +55,7 @@ router.delete(
   isSeller,
   catchAsyncErrors(async (req, res, next) => {
     try {
-      const couponCode = await CoupounCode.findByIdAndDelete(req.params.id);
+      const couponCode = await VoucherCode.findByIdAndDelete(req.params.id);
 
       if (!couponCode) {
         return next(new ErrorHandler("Coupon code dosen't exists!", 400));
@@ -75,7 +75,7 @@ router.get(
   "/get-coupon-value/:name",
   catchAsyncErrors(async (req, res, next) => {
     try {
-      const couponCode = await CoupounCode.findOne({ name: req.params.name });
+      const couponCode = await VoucherCode.findOne({ name: req.params.name });
 
       res.status(200).json({
         success: true,
